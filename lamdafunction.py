3a5fc7b935e2a4ca7a7f94c4373d39cfb2b4151d
Joan Gerard <joan.sebastian.gerard@gmail.com>
2023-07-24 19:34:32 -0400

    Update lamdafunction.py

--- lamdafunction.py
+++ lamdafunction.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 print('Loading Lambda function')
 
 runtime=boto3.Session().client('sagemaker-runtime')
-endpoint_Name='BradTestEndpoint'
+endpoint_Name='pytorch-inference-2023-07-24-16-54-26-788'
 
 def lambda_handler(event, context):
 
